e781ea779135b23b9e9d1d08c116bd70954b5805
domotheawsome <cblappers@gmail.com>
2023-03-21 21:55:36 -0700

    cleaned up search bar, separated into component

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -1,17 +1,20 @@
 import styled from '@emotion/styled/macro'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import TopicItem from './TopicItem'
 
 const InterestAreas = styled.ul`
-    background-color: aliceblue;
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
     list-style: none;
     text-align: center;
     padding-top: 20px;
-`
 
+    border-bottom: 4px double;
+    border-top: 2px solid gray;
+    
+`
 const Topic = styled.li`
     &:hover{
         cursor: pointer;
@@ -25,53 +28,43 @@ const Topic = styled.li`
     }
 `
 
+
+
 function Topics(props) {
     return (
         <>
             <span>
                 <InterestAreas>
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("business")
-                            props.setQueryParam("business")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>Business</p></Topic>
-                    </Link>
-
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("entertainment")
-                            props.setQueryParam("entertainment")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>Entertainment</p></Topic>
-                    </Link>
-
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("general")
-                            props.setQueryParam("general")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>General</p></Topic>
-                    </Link>
-
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("health")
-                            props.setQueryParam("health")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>Health</p></Topic>
-                    </Link>
-
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("science")
-                            props.setQueryParam("science")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>Science</p></Topic>
-                    </Link>
-
-                    <Link to="/">
-                        <Topic onClick={e => {
-                            props.setQuery("sports")
-                            props.setQueryParam("sports")
-                        }}><img src="https://www.w3schools.com/css/paris.jpg" alt="something" /> <p>Sports</p></Topic>
-                    </Link>
-
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"business"}
+                    />
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"entertainment"}
+                    />
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"general"}
+                    />
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"health"}
+                    />
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"science"}
+                    />
+                    <TopicItem
+                        setQuery={props.setQuery}
+                        setQueryParam={props.setQueryParam}
+                        interest={"sports"}
+                    />
                 </InterestAreas>
             </span>
             {/* {props.articles.map((article, idx) => (
