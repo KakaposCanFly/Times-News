f1d6f2251df9c28d3646e311e808cb2ad1ff1a94
Josh Minyard <55771182+JoshyOWashy@users.noreply.github.com>
2023-03-22 11:10:51 -0700

    Josh fixes (#16)
    * search button now redirects to homepage when clicked in article
    
    * spinner is now shown when articles are loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import { useState } from 'react';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
+import { useNavigate } from "react-router-dom";
 import './App.css';
 import Article from './components/Article';
 import MainScreen from './components/MainScreen';
 import Nav from './components/Nav';
 import Topics from './components/Topics';
 import useNewsSearch from './hooks/useNewsSearch';
-import countries from './selections/countries.json';
 import languages from './selections/languages.json';
+
+
+// const searchButton = styled.Link`
+
+// `
 
 function App() {
   const [languageParam, setLanguageParam] = useState("en")
@@ -21,12 +26,13 @@ function App() {
 
   const [articles, loading, error] = useNewsSearch(queryParam, domainParam, languageParam)
 
+  let navigate = useNavigate()
 
   return (
     <>
       <Nav />
       <div >
-        <form className="form--container "onSubmit={e => {
+        <form id="form" className="form--container " onSubmit={e => {
           e.preventDefault()
           //set search params
           console.log("language in app: ", language)
@@ -43,27 +49,27 @@ function App() {
           document.getElementById("domainInput").value = ""
 
         }}>
-          <div class="input-group mb-3">
-  <div class="input-group-prepend ">
-          <input className='form-control form--gap' id="queryInput" type='text' placeholder='Search' onChange={e => {
-            setQuery(e.target.value)
-          }}></input>
-          <input className='form-control form--gap'  id="domainInput" type='text' placeholder='Sources' onChange={e => {
-            setDomain(e.target.value)
-          }}></input>
-          <div className="dropdown">
-          <select className="form--gap btn btn-secondary dropdown-toggle" onChange={
-            e => setLanguage(e.target.value)
-          }>
-            
-            <option  className=" dropdown-menu" value="en" selected>EN</option>
-            {languages.map((code, idx) => (
-              <option  key={idx} value={code.code}>{(code.code).toUpperCase()}</option>
-            ))}
-            
-          </select>
-          </div>
-          {/* <select onChange={
+          <div className="input-group mb-3">
+            <div className="input-group-prepend ">
+              <input className='form-control form--gap' id="queryInput" type='text' placeholder='Search' onChange={e => {
+                setQuery(e.target.value)
+              }}></input>
+              <input className='form-control form--gap' id="domainInput" type='text' placeholder='Sources' onChange={e => {
+                setDomain(e.target.value)
+              }}></input>
+              <div className="dropdown">
+                <select className="form--gap btn btn-secondary dropdown-toggle" onChange={
+                  e => setLanguage(e.target.value)
+                }>
+
+                  <option className=" dropdown-menu" value="en" selected>EN</option>
+                  {languages.map((code, idx) => (
+                    <option key={idx} value={code.code}>{(code.code).toUpperCase()}</option>
+                  ))}
+
+                </select>
+              </div>
+              {/* <select onChange={
             e => setCountry(e.target.value)
           }>
             <option value="us" selected>US</option>
@@ -71,10 +77,10 @@ function App() {
               <option key={idx} value={code.code}>{(code.code).toUpperCase()}</option>
             ))}
           </select> */}
-          {/* <Link to="/"> */}
-          <button type='submit' className="btn btn-primary">Search</button>
-          {/* </Link> */}
-          </div>
+              <button type='submit' className="btn btn-primary" onClick={() => {
+                navigate('/')
+              }}>Search</button>
+            </div>
           </div>
         </form>
       </div>
--- src/components/MainScreen.js
+++ src/components/MainScreen.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled/macro'
 import React from 'react'
 import { Link } from 'react-router-dom'
 import LandingPage from './LandingPage'
+import Spinner from './Spinner'
 
 const ArticleList = styled.ul`
     display: flex;
@@ -10,37 +11,26 @@ const ArticleList = styled.ul`
     list-style: none;
     gap:20px;
 `
-
-const ArticleBox = styled.li`
+const SpinnerDiv = styled.div`
     display: flex;
-    flex-direction: column;
-    margin: 5px;
-    border: solid;
-    border-width: 4px;
-    border-radius: 10px;
-
-    img {
-        width: 200px;
-        height: 150px;
-    }
+    justify-content: center;
 `
 
 function MainScreen(props) {
     console.log("These are the articles: ", props.articles)
     return (
         <>
-
-            {props.articles.length === 0 ? <LandingPage /> : <ArticleList>
+            {props.articles.length === 0 ? <LandingPage /> : props.loading ? <SpinnerDiv> <Spinner id="spinner" /> </SpinnerDiv> : <ArticleList>
                 {props.articles.map((article, idx) => (
-                    <Link key={idx}  to={`/article/${article.title}`} state={article}>
+                    <Link key={idx} to={`/article/${article.title}`} state={article}>
                         <div className="card w card--container">
                             {article.urlToImage ? (
                                 <img className="img--size card-img-top" src={article.urlToImage} alt="poster" />
-                            ) : <img className="img--size card-img-top"src={"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"} alt="na"/>}
+                            ) : <img className="img--size card-img-top" src={"https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930"} alt="na" />}
 
                             <div className="card-body">
-                            <h5 className="card-title">{article.title}</h5>
-                            <p className='card-text fon'>{article.description}</p>
+                                <h5 className="card-title">{article.title}</h5>
+                                <p className='card-text fon'>{article.description}</p>
                             </div>
                         </div>
                     </Link>
